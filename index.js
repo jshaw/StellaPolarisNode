6d5f860e39051bed2e307f53e5767fdcb8403718
Jordan Shaw <jrdnshw@gmail.com>
2018-10-21 14:49:39 -0400

    adding filtering and caching periferals for object look up. Updating last seen on object #2 close #1 close #9

--- index.js
+++ index.js
@@ -9,13 +9,17 @@
 // 
 // ===================
 
+var _ = require('lodash');
 const WebSocket = require('ws');
 const async = require('async');
 const noble = require('noble');
+const util = require('util');
+const setTimeoutPromise = util.promisify(setTimeout);
+const setTimeoutInterval = util.promisify(setInterval);
 
 const allowDuplicates = true;
-// const serviceUUIDs = [];
-const serviceUUIDs = ["1800"];
+const serviceUUIDs = [];
+// const serviceUUIDs = ["1800"];
 
 // Noble BLE Services
 noble.on('stateChange', function(state) {
@@ -31,6 +35,140 @@ function callbackHandler(error){
   console.log("Error Error", error);
 
 }
+
+var timeoutID;
+var save_cache = true;
+var sendID;
+var send_discoveries = false;
+var globalCounter = 0;
+
+var deviceCount = 0;
+
+
+delayedAlert();
+
+
+// var ble_cache = {};
+var ble_cache = [];
+var discoveries = [];
+
+// const setTimeoutPromise = util.promisify(setTimeout);
+
+// clearInterval
+
+// setTimeoutPromise(40, 'foobar').then((value) => {
+//   // value === 'foobar' (passing values is optional)
+//   // This is executed after about 40 milliseconds.
+// });
+
+function delayedAlert() {
+  // timeoutID = setTimeout(alert, 15000, timerCallback);
+  timeoutID = setTimeoutPromise(15000, 'foobar').then((value) => {
+
+
+    // console.log("VALUE????", value);
+
+    // // value === 'foobar' (passing values is optional)
+    // // This is executed after about 40 milliseconds.
+
+
+    save_cache = false;
+    sendDiscoveriesTimer();
+    clearAlert();
+
+
+    console.log("ble_cache");
+    console.log("ble_cache");
+    console.log("ble_cache");
+    console.log("====================");
+    console.log(ble_cache);
+    console.log("====================");
+    console.log("====================");
+    console.log("====================");
+    console.log("====================");
+    console.log("====================");
+
+
+  });
+}
+
+function sendDiscoveriesTimer(){
+
+    sendID = setIntervalPromise(5000, 'foobar').then((value) => {
+
+
+    // console.log("VALUE????", value);
+
+    // // value === 'foobar' (passing values is optional)
+    // // This is executed after about 40 milliseconds.
+
+
+    // save_cache = false;
+    // clearAlert();
+
+
+    console.log("ble_cache");
+    console.log("ble_cache");
+    console.log("ble_cache");
+    console.log("====================");
+    console.log("SEND DISCOVERIES");
+    console.log(discoveries);
+    console.log("====================");
+    console.log("====================");
+    console.log("====================");
+    console.log("====================");
+    console.log("====================");
+
+
+  });
+
+  clearPeriferalLastSeen();
+
+}
+
+function clearPeriferalLastSeen(){
+  _.each(discoveries, function(val, index){
+
+    // if last seen was 30 seconds ago
+    // set it as inactive
+    // maybe we'll remove this from the object
+    // unsure at the moment
+    if(globalCounter - val.lastSeen > 30000){
+      val.active = false;
+    }
+
+  });
+
+}
+
+// function timerCallback(){
+//   save_cache = false;
+//   clearAlert();
+
+
+//   console.log("ble_cache");
+//   console.log("ble_cache");
+//   console.log("ble_cache");
+//   console.log("====================");
+//   console.log(ble_cache);
+//   console.log("====================");
+//   console.log("====================");
+//   console.log("====================");
+//   console.log("====================");
+//   console.log("====================");
+
+
+
+// }
+
+function clearAlert() {
+  clearTimeout(timeoutID);
+}
+
+
+// init Noble BLE 
+initNoble();
+
 
 
 // WebSocket Code
@@ -74,7 +212,14 @@ wss.on('close', function connection(ws, req) {
     //
 });
 
-initNoble();
+
+// sets script global counter
+setInterval(function() {
+
+    globalCounter += 200;
+
+}, 200);
+
 
 // function initNoble(ws){
 function initNoble(){
@@ -91,8 +236,8 @@ function initNoble(){
       console.log('peripheral with ID ' + peripheral.id + ' found');
       var advertisement = peripheral.advertisement;
 
-      console.log('peripheral: ' + peripheral + ' found');
-      console.log('advertisement: ' + advertisement + ' found');
+      // console.log('peripheral: ' + peripheral + ' found');
+      // console.log('advertisement: ' + advertisement + ' found');
       
 
       var localName = advertisement.localName;
@@ -101,6 +246,8 @@ function initNoble(){
       var serviceData = advertisement.serviceData;
       var serviceUuids = advertisement.serviceUuids;
       var rssi = peripheral.rssi;
+
+      var mfd;
 
       if (localName) {
         console.log('  Local Name        = ' + localName);
@@ -112,11 +259,12 @@ function initNoble(){
 
       if (manufacturerData) {
         console.log('  Manufacturer Data = ' + manufacturerData.toString('hex'));
+        mfd = manufacturerData.toString('hex');
       }
 
-      if (serviceData) {
-        console.log('  Service Data      = ' + JSON.stringify(serviceData, null, 2));
-      }
+      // if (serviceData) {
+      //   console.log('  Service Data      = ' + JSON.stringify(serviceData, null, 2));
+      // }
 
       if (serviceUuids) {
         console.log('  Service UUIDs     = ' + serviceUuids);
@@ -127,12 +275,73 @@ function initNoble(){
       }
 
       // sendThroughWebSocket(_ws, rssi);
-        if (_ws !== null) {
-          console.log("GOOD _ws: ", _ws);
-          _ws.send(rssi);
-        } else {
-          console.log("BAD _ws: NO CONNECTION");
+      if (_ws !== null) {
+        console.log("GOOD _ws: ", _ws);
+        _ws.send(rssi);
+      } else {
+        console.log("BAD _ws: NO CONNECTION");
+      }
+
+
+      if(save_cache == true){
+
+        if (mfd != undefined){
+
+          if(_.indexOf(ble_cache, mfd) == -1){
+            deviceCount++;
+            ble_cache.push(mfd);
+          }
         }
+
+      } else {
+        // console.log("ble_cache", ble_cache);
+
+        // Check to see if it's in the cache
+        if(_.indexOf(ble_cache, mfd) == -1){
+          // Checks to see if the device is in the discovery object
+          if(_.has(discoveries, mfd)){
+            // do update here for the current rssi values
+
+            console.log("TO USE: HAS THIS PERIFERAL IN TO USE LIST");
+
+            discoveries[mfd].rssi = rssi;
+            discoveries[mfd].lastSeen = globalCounter;
+
+            console.log("discoveries: ", discoveries);
+            console.log(" ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+            console.log(" ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+            console.log(" ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+            console.log(" ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+            console.log(" ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+
+            // update the object's rssi periferal
+            // update last seen for garbage cleaning at end of script
+
+          } else {
+            // add peripheral to the discoveries ID'd under the MFD
+            // discoveries[mfd] = peripheral;
+            deviceCount++;
+
+            discoveries[mfd] = {
+              deviceUID: "_"+device
+              deviceCount: deviceCount,
+              mfd: mfd,
+              uuid: peripheral.uuid,
+              localName: localName,
+              serviceData: JSON.stringify(serviceData, null, 2),
+              serviceUuids: serviceUuids,
+              rssi: rssi,
+              active: true,
+              discovered: globalCounter,
+              lastSeen: globalCounter
+            };
+
+          }
+
+        }
+
+      }
+
 
       console.log();
 
